df5c935586ec9878e78f9c9d35ec5fbdc8b4a22e
jkim- <jaehongkim86@gmail.com>
2018-08-02 03:29:44 +0000

    Setup TravisCI.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run `jekyll serve`, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
         "url": "http://jaehongkim.xyz/jekyll/update/2018/08/01/welcome-to-jekyll.html",
         "teaser":null}]
