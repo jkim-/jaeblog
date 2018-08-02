0038942ff3ced94b6c5759854e98b247184c13a9
jkim- <jaehongkim86@gmail.com>
2018-08-02 19:24:23 +0000

    Added robots.txt.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "How to unsubscribe from Caltech marketplace mailing list",
         "excerpt":"1. Access campus network Access the campus network (either byVPN or connecting to campus Wi-Fi). 2. Go to the mailing list administration page Next, go tohttps://lists.caltech.edu/listinfo/marketplace,and enter your email at the bottom next to Unsubscribe or edit options. 3. Unsubscribe! Simply click the unsubscribe button and you’re done! ","categories": ["Caltech"],
         "tags": [],
-        "url": "http://142.93.27.23:4000/caltech/2018/08/02/how-to-unsubscribe-from-caltech-marketplace.html",
+        "url": "http://jaehongkim.xyz/caltech/2018/08/02/how-to-unsubscribe-from-caltech-marketplace.html",
         "teaser":null}]
